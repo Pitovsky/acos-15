56cd1904d862343410e68e8cca135b5aff566751
Anrew Karyamin <algebroid.2011@gmail.com>
2015-03-11 17:37:43 +0300

    More comments

--- 11_karamin/01_hashtable/main.c
+++ 11_karamin/01_hashtable/main.c
@@ -9,11 +9,11 @@ const int MAXCOUNT = 1000, tsize = 5, MaxStringSize = 100;
 
 int main()
 {
-
 	FILE* in;
 	in = fopen("keys.txt", "r");
 	if (in == NULL) return 1;
 
+//________Filling table from input.txt________
 //Entering Data with a key
 	int count, i/*, DataTypeSize*/;
 	//fscanf(in, "%d", &DataTypeSize);
@@ -24,41 +24,41 @@ int main()
 		/*fscanf(in, "%d", &input[i]->data);*/ FileReadString(in, input[i]->KEY);
 	}
 	count = i;
-//C-Strings
-
-
 
 	struct node** h = CreateTable(tsize);
 	for (i = 0; i < count; ++i)
 		Insert(h, tsize, input[i]);
 	
 	PrintTable(h, tsize);
+//________Filling table from input.txt________
 
-
-	//Inserting
-	//struct data *ins = (struct data*)malloc(sizeof(struct data));
+//____________Inserting_______________
+	struct data *ins = (struct data*)malloc(sizeof(struct data));
 	/*ins->data = ()malloc();
 	printf("\n\nEnter inserting data --> ");
 	scanf("%d", ins->data);*/
-	//printf("\n\nEnter data key--> ");
-	//key temp = (key)malloc(MaxStringSize*sizeof(char));
-	//ReadString(temp);
-	//ins->KEY = temp;
-	//Insert(h, tsize, ins);
+	printf("\n\nEnter data key--> ");
+	key temp = (key)malloc(MaxStringSize*sizeof(char));
+	ReadString(temp);
+	ins->KEY = temp;
+	Insert(h, tsize, ins);
 
-	//printf("\n\n");
-	//PrintTable(h, tsize);
+	printf("\n\n");
+	PrintTable(h, tsize);
+//____________Inserting_______________
 
-	//key del = (key)malloc(100 * sizeof(char));
-	//printf("\n\nEnter deleting string --> ");
-	//scanf("%s", del);
-	//Delete(h, tsize, del);
+//____________Deleting________________
+	key del = (key)malloc(100 * sizeof(char));
+	printf("\n\nEnter deleting string --> ");
+	scanf("%s", del);
+	Delete(h, tsize, del);
 
-	//printf("\n\n");
-	//PrintTable(h, tsize);
-	
-	//ClearTable(h, tsize);
-	//DeleteTable(h, tsize);
+	printf("\n\n");
+	PrintTable(h, tsize);
+//____________Deleting________________
+
+	ClearTable(h, tsize);
+	DeleteTable(h, tsize);
 
 	return 0;
 }
