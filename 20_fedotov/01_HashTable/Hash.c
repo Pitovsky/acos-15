521bf06f94062e3da6eba15c01ac26c392c82291
Semyon Fedotov <simon23rus@gmail.com>
2015-03-11 21:17:20 +0300

    Version 1: need update tests

---
+++ 20_fedotov/01_HashTable/Hash.c
@@ -0,0 +1,167 @@
+//
+//  Hash.c
+//  ACOS_HashTable
+//
+//  Created by Simon Fedotov on 26.02.15.
+//  Copyright (c) 2015 Simon23Rus. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Hash.h"
+
+
+struct Table* CreateTable(int size) {
+    struct Table* NewTable = (struct Table*) malloc(size * sizeof(struct Table));
+    NewTable->size = size;
+    NewTable -> NumberOfElements = 0;
+    NewTable->OurTable = (struct listNode**) malloc(size * sizeof(struct listNode*));
+    for(int i = 0; i < size; ++i) {
+        NewTable -> OurTable[i] = NULL;
+    }
+    return NewTable;
+}
+
+int Hash(char* key, int size) {
+    int result = 0;
+    for(int i = 0; i < strlen(key); ++i) {
+        result += (int)(key[i]);
+    };
+    result += key[0] + key[strlen(key) - 1];
+    return result % size;
+}
+
+void insertToTable(struct Table* DatTable, struct Data DatData) {
+    ++(DatTable -> NumberOfElements);
+    struct listNode* CheckList = NULL;
+    if(CheckList != NULL) {
+        CheckList -> OurData = DatData;
+        return;
+    }
+    if(DatTable -> OurTable[Hash(DatData . key, DatTable -> size)] == NULL) {
+        struct listNode* NewElement = (struct listNode*)malloc(sizeof(struct listNode));
+        NewElement -> OurData = DatData;
+        NewElement -> nextNode = NULL;
+        DatTable -> OurTable[Hash(DatData . key, DatTable -> size)] = NewElement;
+    }
+    else {
+        struct listNode* NewElement = (struct listNode*)malloc(sizeof(struct listNode));
+        NewElement -> OurData.data = DatData . data;
+        NewElement -> OurData.key = DatData . key;
+        NewElement -> nextNode = DatTable -> OurTable[Hash(DatData . key, DatTable -> size)];
+        DatTable -> OurTable[Hash(DatData . key, DatTable -> size)] = NewElement;
+    }
+}
+
+unsigned short Contains(struct Table* DatTable, struct Data* DatData) {
+    struct listNode* OurPosition = (struct listNode*)malloc(sizeof(struct listNode*));
+    OurPosition = DatTable -> OurTable[Hash(DatData -> key, DatTable -> size)];
+    //    if(OurPosition -> OurData.key != DatData -> key || (OurPosition ->OurData.key == DatData ->key && OurPosition -> OurData.data != DatData ->data)) {
+    //        return 0;
+    //    }
+    while (OurPosition != NULL) {
+        if(!strcmp(OurPosition ->OurData.key , DatData ->key))
+            return 1;
+        else
+            OurPosition = OurPosition -> nextNode;
+    }
+    return 0;
+}
+
+void PrintList(struct listNode* dat) {
+    while (dat != NULL) {
+        printf("%d\n", dat -> OurData.data);
+        dat = dat -> nextNode;
+    }
+}
+
+struct listNode* Search(struct Table* DatTable, struct Data* DatData) {
+    struct listNode* OurPosition = (struct listNode*)malloc(sizeof(struct listNode*));
+    OurPosition = DatTable -> OurTable[Hash(DatData -> key, DatTable -> size)];
+    while (OurPosition != NULL) {
+        if(!strcmp(OurPosition ->OurData.key , DatData ->key)  && OurPosition -> OurData.data == DatData ->data)
+            return OurPosition;
+        else
+            OurPosition = OurPosition -> nextNode;
+    }
+    return NULL;
+}
+
+struct listNode* SearchPar(struct Table* DatTable, struct Data* DatData) {
+    struct listNode* OurPosition = (struct listNode*)malloc(sizeof(struct listNode*));
+    OurPosition = DatTable -> OurTable[Hash(DatData -> key, DatTable -> size)];
+    if(OurPosition == NULL)
+        return NULL;
+    if(OurPosition -> OurData.key == DatData -> key && OurPosition -> OurData.data == DatData -> data) {
+        return NULL;
+    }
+    while ((OurPosition -> nextNode) != NULL) {
+        if(!strcmp(OurPosition -> nextNode ->OurData.key , DatData ->key)  && OurPosition -> nextNode -> OurData.data == DatData ->data)
+            return OurPosition;
+        else
+            OurPosition = OurPosition -> nextNode;
+    }
+    return NULL;
+}
+
+unsigned short int Delete(struct Table* DatTable, struct Data* DatData) {
+    struct listNode* ToDelete = (struct listNode*)malloc(sizeof(struct listNode*));
+    struct listNode* ParentDel = (struct listNode*)malloc(sizeof(struct listNode*));
+    struct listNode* tmp = (struct listNode*)malloc(sizeof(struct listNode*));
+    ToDelete = Search(DatTable, DatData);
+    if (ToDelete == NULL) {
+        return 0;
+    }
+    ParentDel = SearchPar(DatTable, DatData);
+    tmp = ToDelete -> nextNode;
+    --DatTable -> NumberOfElements;
+    if(ParentDel == NULL) {
+        DatTable -> OurTable[Hash(DatData -> key, DatTable -> size)] = ToDelete -> nextNode;
+        free(ToDelete);
+        return 1;
+    }
+    ParentDel -> nextNode = tmp;
+    free(ToDelete);
+    
+    return 1;
+}
+
+
+
+void Deletion(struct listNode* OurNode) {
+    if(OurNode != NULL) {
+    struct listNode* Next = OurNode -> nextNode;
+        free(OurNode);
+        OurNode = NULL;
+    Deletion(Next);
+    }
+}
+
+void DeleteTable(struct Table* ThisTable) {
+    for(int i = 0; i < ThisTable -> size; ++i) {
+        if(ThisTable -> OurTable[i] != NULL) {
+            Deletion(ThisTable -> OurTable[i]);
+        }
+    }
+    ThisTable -> NumberOfElements = 0;
+}
+
+struct Table* MoveToTable(struct Table* MyFirst, struct Table* MySecond) {
+    for(int i = 0; i < MyFirst -> size; ++i) {
+        if(MyFirst -> OurTable[i] != NULL) {
+            struct listNode* ToMove = (struct listNode*)malloc(sizeof(struct listNode*));
+            ToMove = MyFirst -> OurTable[i];
+            while (ToMove != NULL) {
+                insertToTable(MySecond, MyFirst -> OurTable[i] -> OurData);
+                ToMove = ToMove -> nextNode;
+            }
+            
+        }
+    }
+    DeleteTable(MyFirst);
+    return MySecond;
+}
+
+
---
+++ 20_fedotov/01_HashTable/testfile.c
@@ -0,0 +1,80 @@
+//
+//  testfile.c
+//  ACOS_HashTable
+//
+//  Created by Simon Fedotov on 27.02.15.
+//  Copyright (c) 2015 Simon23Rus. All rights reserved.
+//
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "Hash.h"
+
+
+int main() {
+    FILE *myFile = fopen("/Users/semenfedotov/Desktop/ACOS/ACOS_HashTable/ACOS_HashTable/test2.txt", "rb"); //У меня тольkо так возможно
+    FILE *myOut =  fopen("/Users/semenfedotov/Desktop/ACOS/ACOS_HashTable/ACOS_HashTable/stud_ans.txt", "w");//подключить файлы
+    int NewSize;
+    fscanf(myFile, "%d", &NewSize);
+    struct Table* NewTable = CreateTable(NewSize);
+    struct Table* DatTableNew = CreateTable((NewTable -> size )* 2);
+    struct Table* MyTable = CreateTable(0);
+    MyTable = NewTable;
+    for(int i = 0; i < NewSize; ++i) {
+        int FirstData;
+        char* FirstKey = (char*) malloc(255 * sizeof(char));
+        fscanf(myFile, "%s%d", FirstKey, &FirstData);
+        struct Data* NewData = (struct Data*)malloc(sizeof(struct Data));
+        NewData -> data = FirstData;
+        NewData -> key = FirstKey;
+        insertToTable(MyTable, *NewData);
+        if(MyTable -> NumberOfElements >= (4.0 * MyTable -> size) / 3.0) {
+            struct Table* DatTable = CreateTable(NewTable -> size * 2);
+            DatTableNew = MoveToTable(NewTable, DatTable);
+            MyTable = DatTableNew;
+        }
+        
+    }
+    int FirstData = 0;
+    char* FirstKey = (char*) malloc(255 * sizeof(char));
+    int counter = 0;
+    FILE* ToDelete = fopen("/Users/semenfedotov/Desktop/ACOS/ACOS_HashTable/ACOS_HashTable/test2.txt", "rb");
+    fscanf(ToDelete, "%d", &NewSize);
+    while(strcmp(FirstKey, "/////")) {
+        struct Data* NewData = (struct Data*)malloc(sizeof(struct Data));
+        ++counter;
+        if(!strcmp(FirstKey, "/////"))
+            break;
+        fscanf(ToDelete, "%s%d", FirstKey, &FirstData);
+        NewData -> data = FirstData;
+        NewData -> key = FirstKey;
+        if(counter % 6 == 1) {
+            Delete(MyTable, NewData);
+        }
+    }
+    
+    struct Data* NewData = (struct Data*)malloc(sizeof(struct Data));
+    FILE* ToInput = fopen("/Users/semenfedotov/Desktop/ACOS/ACOS_HashTable/ACOS_HashTable/test2.txt", "rb");
+    fscanf(ToInput, "%d", &NewSize);
+    FirstData = 0;
+    FirstKey = (char*) malloc(255 * sizeof(char));
+    while(strcmp(FirstKey, "/////")) {
+    fscanf(ToInput, "%s%d", FirstKey, &FirstData);
+        if(!strcmp(FirstKey, "/////"))
+            break;
+    NewData -> data = FirstData;
+    NewData -> key = FirstKey;
+        if(Contains(MyTable, NewData)) {
+            fprintf(myOut, "%s\n", "YES");
+        }
+        else {
+                fprintf(myOut, "%s\n", "NO");
+        }
+    }
+    fclose(myFile);
+    fclose(myOut);
+    DeleteTable(NewTable);
+    DeleteTable(DatTableNew);
+    printf("Everything is alright!\n");
+    return 0;
+}
