ddd2ffac9b53b7da8d0efedc829f56490bda56c0
Daniel Riazanovskiy <riazanovskiy@gmail.com>
2015-03-23 12:03:34 +0300

    new tests

--- 16_ryazanovskiy/01_hashtable/main.c
+++ 16_ryazanovskiy/01_hashtable/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -121,6 +122,9 @@ int main()
 
     insertToHashTable(table, "Stepan", v(15));
     eraseFromHashTable(table, "Stepan");
+    assert(0 == findInHashTable(table, "Danila"));
+    assert(0 == findInHashTable(table, "Stepan"));
+
     srand(2);
     for (int i = 0; i < 100000; i++)
     {
@@ -136,6 +140,7 @@ int main()
         char string[10] = "";
         for (int k = 0; k < 9; k++)
             string[k] = (char)(rand() & 0xff);
+        printf("%i\n", (int)(findInHashTable(table, string) == v(i + 42)));
         eraseFromHashTable(table, string);
     }
 
