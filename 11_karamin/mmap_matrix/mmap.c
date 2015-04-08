3b1589f6b2c0d0fabdb4727629d2ca04f526fd4f
Andrew Karyamin <algebroid.2011@gmail.com>
2015-04-08 14:41:58 +0300

    В mmap_matrix добавлены проверки на корректность и множество комментариев.

--- 11_karamin/mmap_matrix/mmap.c
+++ 11_karamin/mmap_matrix/mmap.c
@@ -10,30 +10,74 @@
 
 int main(int argc, char** argv)
 {
-	if (argc < 2)
+//Элементарная проверка на корректность входных данных.
+	if (argc < 3)
 	{
-		printf("Not enough arguments\n");
+		printf("Wrong input\n");
+		return 1;
+	}
+
+//Открываем файл для чтения/записи с матрицей в бинарном виде
+	int infd;
+	if ((infd = open(argv[1], O_RDWR)) < 0)
+	{
+		printf("Can't open %s for read/write\n", argv[1]);
+		return 1;
+	}
+
+//Сбор сведений о файле с матрицей в бинарном виде
+	struct stat fileinfo;
+	if (stat(argv[1], &fileinfo) < 0)
+	{
+		printf("Stat error.\n");
 		return 1;
 	}
 	
-	int infd;
-	infd = open(argv[1], O_RDWR);
-	
-	struct stat fileinfo;
-	stat(argv[1], &fileinfo);
-	
+//Здесь вычисляется количество байт в файле,
+//которые должны быть отображены в адресное пространство программы.
+	int newfilesize;
 	int stlb = atoi(argv[2]), strk;
 	if (argc >= 4)
 		strk = atoi(argv[3]);
 	else
 		strk = fileinfo.st_size / (sizeof(double)*stlb);
+	newfilesize = strk*stlb*sizeof(double);
 	
-	
+//Здесь проверка на то, как размер входного файла соответствует запрошенным
+//пользователем данным. Если пользователь запросил больше, чем записана матрица
+//в файле, то файл дополнить необходимым количеством байт и заполнить нулями.
+//newfilesize - это как раз количество байт, которые будут отображены в
+//адресное пространство программы.
 	double *matrix;
-	matrix = mmap(0, sizeof(double) * strk * stlb, PROT_READ | PROT_WRITE, MAP_SHARED, infd, 0);
-	
-	
-	
+	if (newfilesize <= fileinfo.st_size)	//Если в файле достаточно данных, запрошенных пользователем.
+	{
+		if((matrix = mmap(0, newfilesize, PROT_READ | PROT_WRITE, MAP_SHARED, infd, 0)) == MAP_FAILED)
+		{
+			printf("mmap failed.\n");
+			return 1;
+		}
+	}
+	else	//Если в файле недостаточно данных, до приписать в конец сколько нужно и отобразить
+	{
+		FILE *temp; int i, firstnewpos = fileinfo.st_size;
+		temp = fopen(argv[1], "a");
+//Добавляем необходимое количество байт в конец файла.		
+		for (i = 0; i < newfilesize - fileinfo.st_size; ++i)
+			fprintf(temp, "%c", '0');
+		fclose(temp);
+//Опять собираем информацию о файле
+		stat(argv[1], &fileinfo);
+//И только теперь отображаем этот файл в память (matrix.txt)
+		if((matrix = mmap(0, newfilesize, PROT_READ | PROT_WRITE, MAP_SHARED, infd, 0)) == MAP_FAILED)
+		{
+			printf("mmap failed.\n");
+			return 1;
+		}
+//Инициализация по умолчанию нулями дополненного пространства файла
+		for (i = firstnewpos; i < strk*stlb; ++i)
+			matrix[i] = 0;
+	}
+		
 //Отсюда начинается реализация самих команд	
 	char *command = (char*)malloc(MAX_COMMAND_SIZE*sizeof(char));
 	scanf("%s", command);
@@ -124,6 +168,5 @@ int main(int argc, char** argv)
 
 ● swap x y ­­­ переставить соотв. строки
 */
-	
 	return 0;
 }
