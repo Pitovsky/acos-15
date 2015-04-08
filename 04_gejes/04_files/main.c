7b2b26c108f13b7559ca3570d9e68df8a286889f
Gezhes Petr <pet.pohta@gmail.com>
2015-04-08 17:49:20 +0300

    Files: added additional task and fixed bugs

--- 04_gejes/04_files/main.c
+++ 04_gejes/04_files/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -15,10 +17,18 @@ int strToInt(char* str)
     }
     return ret;
 }
-int outWordDir(char* path, int recDeep, int jumpLink)
+
+char* visitedLinks[1000];
+int visitedLinkCount = 0;
+
+int outWordDir(char* unabpath, int recDeep, int jumpLink)
 {
     int i = 0;
     char word[262143];
+    char path[256];
+    char entryPath[256];
+    realpath(unabpath, path);
+    strcpy(entryPath, path);
     struct dirent** namelist;
     int inDirCount = scandir(".", &namelist, NULL, alphasort);
     printf("Now in directory: %s, else %d levels\n", path, recDeep);
@@ -30,8 +40,10 @@ int outWordDir(char* path, int recDeep, int jumpLink)
         {
             getcwd(path, sizeof(path));
             outWordDir(path, recDeep - 1, jumpLink);
-            chdir("..");
-            getcwd(path, sizeof(path));
+            chdir(entryPath);
+            strcpy(path, entryPath);
+            //realpath(unabpath, path);
+            printf("Return to directory: %s, else %d levels\n", path, recDeep);
         }
         else
         {
@@ -39,9 +51,30 @@ int outWordDir(char* path, int recDeep, int jumpLink)
             lstat(namelist[i]->d_name,&st);
             if (S_ISLNK(st.st_mode))
             {
-                printf("%s - symlink\n", namelist[i]->d_name);
+                char realPath[256];
+                realpath(namelist[i]->d_name, realPath);
+                printf("%s - symlink to %s\n", namelist[i]->d_name, realPath);
                 if (jumpLink == 0)
                     continue;
+                if (jumpLink == -1)
+                {
+                    int j;
+                    int visited = 0;
+                    for (j = 0; j < visitedLinkCount; ++j)
+                        if (strcmp(visitedLinks[j], realPath) == 0)
+                            ++visited;
+                    if (visited != 0)
+                    {
+                        printf("and I visited it, see upper\n");
+                        continue;
+                    }
+                    else
+                    {
+                        visitedLinks[visitedLinkCount] = (char*)malloc(strlen(realPath)*sizeof(char));
+                        strcpy(visitedLinks[visitedLinkCount], realPath);
+                        ++visitedLinkCount;
+                    }
+                }
             }
             FILE* fin = fopen(namelist[i]->d_name, "r");
             if (fin == NULL)
@@ -75,12 +108,19 @@ int main(int argc, char** argv)
             recDeep = strToInt(argv[i + 1]) - 1;
 
     for (i = 0; i < argc; ++i)
+        if (strcmp(argv[i], "-c") == 0)
+            jumpLink = -1;
+
+    for (i = 0; i < argc; ++i)
         if (strcmp(argv[i], "-s") == 0)
             jumpLink = 0;
 
     chdir(path);
     printf("Ok, I will try it. %d recoursion levels?\n", recDeep);
     outWordDir(path, recDeep, jumpLink);
+
+    for (i = 0; i < visitedLinkCount; ++i)
+        free(visitedLinks[i]);
 
     return 0;
 }
