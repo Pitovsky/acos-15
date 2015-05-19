e4daa087b742323c64d7e6781cbcecb9068f253a
Serafima Vruchtel <vruchtel.serafima@yandex.ru>
2015-05-20 00:55:27 +0300

    Update task 11

--- 03_vrutchel/11_nnt/Client/main.c
+++ 03_vrutchel/11_nnt/Client/main.c
@@ -1,4 +1,6 @@
-/*аргументом argv[1] программе должен передаваться адрес сервера*/
+/*аргументом argv[1] программе должен передаваться адрес сервера
+* аргументом argv[2] программе должен передаваться путь до файла
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,14 +8,34 @@
 #include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define PORT 2306
+
+// возвращает длину файла по его пути (или тупо имени файла)
+int fileNameLengthByPath(const char * filePath) {
+    if(filePath == NULL)
+        return 0;
+
+    if(strrchr(filePath, '/') == NULL)
+        return strlen(filePath);
+
+    int lastSlashPosition = (int)(strrchr(filePath, '/') - filePath);
+    return strlen(filePath) - lastSlashPosition - 1;
+}
 
 int main(int argc, char **argv)
 {
     //если аргумент не получен, то, видимо, сваливаем (ну или ещё как вариант можно попросить ввести его с клавиатуры, но это потом)
     if(argc < 2) {
         printf("No server ip\n");
+        exit(-1);
+    }
+
+     if(argc < 3) {//В argv[2] будем хранить адрес файла, который надо открыть, и содержимое которого надо отправить на сервер
+        printf("No file address\n");
         exit(-1);
     }
 
@@ -40,21 +62,35 @@ int main(int argc, char **argv)
         exit(errno);
     }
 
-    //теперь создаём сообщение, которое надо будет переслать
-    int name_length, info_length;
-    printf("Enter the length of file-name: ");
-    scanf("%d", &name_length);
+    //сообщение у нас будет создаваться: открываем файл и формируем сообщение
+    int fd = open(argv[2], O_RDONLY);
+    if(fd == -1) {
+        perror("Problems with opening file");
+        exit(errno);
+    }
+
+    //здесь определим размер открытого файла
+    struct stat info_file;
+    fstat(fd, &info_file);
+    int info_length = info_file.st_size;//длина содержимого
+
+    //получаем длину имени файла
+    int name_length = fileNameLengthByPath(argv[2]);
+
+    //получаем имя файла
     char name[name_length];
-    printf("Enter the name of file: ");
-    scanf("%s", name);
-    printf("Enter the length of information: ");
-    scanf("%d", &info_length);
+    memcpy(name, argv[2] + strlen(argv[2]) - name_length, name_length);
+
+    //получаем содержимое файла
     char info[info_length];
-    printf("Enter the information: ");
-    scanf("%s", info);
+    int res = read(fd, info, info_length);
+    if(res < 0) {
+        perror("Problems with reading from file");
+        exit(errno);
+    }
 
     //потом отправляем сообщение серверу
-    int res = send(sock, (char*)(&name_length), sizeof(int), 0);
+    res = send(sock, (char*)(&name_length), sizeof(int), 0);
     if(res == -1) {
         perror("Problems with sending the length of file-name");
         exit(errno);
@@ -75,6 +111,7 @@ int main(int argc, char **argv)
         exit(errno);
     }
 
+    close(fd);
     close(sock);
     return 0;
 }
